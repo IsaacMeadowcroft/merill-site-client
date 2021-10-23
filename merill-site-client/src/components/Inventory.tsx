4e870035f2845100ba11cdea4efb3960ba706c46
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-23 16:38:42 -0400

    Got data from database

--- merill-site-client/src/components/Inventory.tsx
+++ merill-site-client/src/components/Inventory.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from "react";
-import { IShopItems } from "./interfaces";
+import { IShopItems, TShopItem } from "./interfaces";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 export const Inventory: FC<IShopItems> = (props) => {
   if (props.shopItems != undefined) {
-    return <Container></Container>;
+    return (
+      <Container>
+        {props.shopItems.map((item: TShopItem) => (
+          <Row key={item.id}>{item.description}</Row>
+        ))}
+      </Container>
+    );
   } else {
     return <></>;
   }
